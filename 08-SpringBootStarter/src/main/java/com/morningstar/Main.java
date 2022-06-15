f8319f6f5e7b8f1456481744d24b2ddff352ac11
Mahesh Mutthe <maheshmutthe@gmail.com.com>
2022-06-15 16:59:50 +0530

    m2

---
+++ 08-SpringBootStarter/src/main/java/com/morningstar/Main.java
@@ -0,0 +1,13 @@
+package com.morningstar;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
